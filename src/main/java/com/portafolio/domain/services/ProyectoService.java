d73352a6e02288438aad322648f986d16840fecf
ALABERTUS <alabertus@gmail.com>
2023-09-08 17:55:40 +0200

    nada

--- src/main/java/com/portafolio/domain/services/ProyectoService.java
+++ src/main/java/com/portafolio/domain/services/ProyectoService.java
@@ -1,14 +1,1 @@
-package com.portafolio.domain.services;
 
-import com.portafolio.domain.models.Proyecto;
-import com.portafolioe.infra.repositories.ProyectoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
-
-@Service
-
-public class ProyectoService {
-}
