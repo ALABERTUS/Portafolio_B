e81e8be3ca1521780de73a366e16d2079ae686bc
ALABERTUS <alabertus@gmail.com>
2023-09-22 14:30:38 +0200

    Cors

--- src/main/java/com/portafolio/apiControllers/ProyectoControllers.java
+++ src/main/java/com/portafolio/apiControllers/ProyectoControllers.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5174")
 @RestController
 @RequestMapping(path = "api/v1/portafolio")
 
